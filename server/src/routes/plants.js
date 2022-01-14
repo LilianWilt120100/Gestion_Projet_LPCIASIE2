c246a729415050e71f5114e8621216d476858ddd
oxypomme <oxytom8@gmail.com>
2022-01-14 13:07:41 +0100

    feat(backend): :goal_net: Added error management

--- server/src/routes/plants.js
+++ server/src/routes/plants.js
@@ -14,13 +14,20 @@ const router = Router();
  * @apiSuccess {array} result The list
  */
 router.get("/", (req, res) => {
-	res.json(
-		db.get("plants").map(({ id, name, latin_name }) => ({
-			id,
-			name,
-			latin_name,
-		}))
-	);
+	try {
+		res.json(
+			db.get("plants").map(({ id, name, latin_name }) => ({
+				id,
+				name,
+				latin_name,
+			}))
+		);
+	} catch (error) {
+		res.status(500).json({
+			status: 500,
+			message: error,
+		});
+	}
 });
 
 /**
@@ -32,32 +39,39 @@ router.get("/", (req, res) => {
  * @apiSuccess {object} result The new plant
  */
 router.post("/", async (req, res) => {
-	const { name, latin_name, description, images, pollen, nectar } = req.body;
-	// Check duplicates
-	if (
-		[...db.get("plants")].find(
-			(p) => p.name === name && p.latin_name === latin_name
-		)
-	) {
-		res.status(409).json({
-			status: 409,
-			message: "Plant already exist",
+	try {
+		const { name, latin_name, description, images, pollen, nectar } = req.body;
+		// Check duplicates
+		if (
+			[...db.get("plants")].find(
+				(p) => p.name === name && p.latin_name === latin_name
+			)
+		) {
+			res.status(409).json({
+				status: 409,
+				message: "Plant already exist",
+			});
+		} else {
+			// Putting plant in DB
+			const plant = {
+				id: uuidv4(),
+				name,
+				latin_name,
+				description,
+				images,
+				pollen,
+				nectar,
+				creationDate: new Date(),
+				editedDate: new Date(),
+			};
+			await db.get("plants").push(plant).write();
+			res.status(201).json(plant);
+		}
+	} catch (error) {
+		res.status(500).json({
+			status: 500,
+			message: error,
 		});
-	} else {
-		// Putting plant in DB
-		const plant = {
-			id: uuidv4(),
-			name,
-			latin_name,
-			description,
-			images,
-			pollen,
-			nectar,
-			creationDate: new Date(),
-			editedDate: new Date(),
-		};
-		await db.get("plants").push(plant).write();
-		res.status(201).json(plant);
 	}
 });
 
@@ -70,14 +84,21 @@ router.post("/", async (req, res) => {
  * @apiSuccess {object} result The plant
  */
 router.get("/:plantId", async (req, res) => {
-	const { plantId } = req.params;
-	const plant = [...db.get("plants")].find(({ id }) => plantId === id);
-	if (plant) {
-		res.json(plant);
-	} else {
-		res.status(404).json({
-			status: 404,
-			message: "Plant not found",
+	try {
+		const { plantId } = req.params;
+		const plant = [...db.get("plants")].find(({ id }) => plantId === id);
+		if (plant) {
+			res.json(plant);
+		} else {
+			res.status(404).json({
+				status: 404,
+				message: "Plant not found",
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			status: 500,
+			message: error,
 		});
 	}
 });
@@ -91,40 +112,47 @@ router.get("/:plantId", async (req, res) => {
  * @apiSuccess {object} result The plant
  */
 router.put("/:plantId", async (req, res) => {
-	const { plantId } = req.params;
-	const { name, latin_name, description, images, pollen, nectar } = req.body;
-	// Check if exist
-	const plants = [...db.get("plants")];
-	const index = plants.findIndex(({ id }) => id === plantId);
-	if (index >= 0) {
-		// Check for duplicates
-		const indexDup = plants.findIndex(
-			(p) => p.name === name && p.latin_name === latin_name
-		);
-		if (indexDup <= 0 || indexDup === index) {
-			const plant = {
-				id: plantId,
-				name,
-				latin_name,
-				description,
-				images,
-				pollen,
-				nectar,
-				creationDate: plants[index].creationDate,
-				editedDate: new Date(),
-			};
-			await db.get("plants").splice(index, 1, plant).write();
-			res.json(plant);
+	try {
+		const { plantId } = req.params;
+		const { name, latin_name, description, images, pollen, nectar } = req.body;
+		// Check if exist
+		const plants = [...db.get("plants")];
+		const index = plants.findIndex(({ id }) => id === plantId);
+		if (index >= 0) {
+			// Check for duplicates
+			const indexDup = plants.findIndex(
+				(p) => p.name === name && p.latin_name === latin_name
+			);
+			if (indexDup <= 0 || indexDup === index) {
+				const plant = {
+					id: plantId,
+					name,
+					latin_name,
+					description,
+					images,
+					pollen,
+					nectar,
+					creationDate: plants[index].creationDate,
+					editedDate: new Date(),
+				};
+				await db.get("plants").splice(index, 1, plant).write();
+				res.json(plant);
+			} else {
+				res.status(404).json({
+					status: 404,
+					message: "Plant with same name or latin_name already exist",
+				});
+			}
 		} else {
 			res.status(404).json({
 				status: 404,
-				message: "Plant with same name or latin_name already exist",
+				message: "Plant not found",
 			});
 		}
-	} else {
-		res.status(404).json({
-			status: 404,
-			message: "Plant not found",
+	} catch (error) {
+		res.status(500).json({
+			status: 500,
+			message: error,
 		});
 	}
 });
@@ -138,26 +166,33 @@ router.put("/:plantId", async (req, res) => {
  * @apiSuccess {image/png} result The QRCode
  */
 router.get("/:plantId/qrcode", async (req, res) => {
-	const { plantId } = req.params;
-	const plant = [...db.get("plants")].find(({ id }) => plantId === id);
-	if (plant) {
-		const base64 = await generateQR(plant);
-		// Encoding base64 into Buffer
-		const qrcode = Buffer.from(
-			// Removing base64 descriptor
-			base64.replace(/^.*,/, ""),
-			"base64"
-		);
+	try {
+		const { plantId } = req.params;
+		const plant = [...db.get("plants")].find(({ id }) => plantId === id);
+		if (plant) {
+			const base64 = await generateQR(plant);
+			// Encoding base64 into Buffer
+			const qrcode = Buffer.from(
+				// Removing base64 descriptor
+				base64.replace(/^.*,/, ""),
+				"base64"
+			);
 
-		res.writeHead(200, {
-			"Content-Type": "image/png",
-			"Content-Length": qrcode.length,
-		});
-		res.end(qrcode);
-	} else {
-		res.status(404).json({
-			status: 404,
-			message: "Plant not found",
+			res.writeHead(200, {
+				"Content-Type": "image/png",
+				"Content-Length": qrcode.length,
+			});
+			res.end(qrcode);
+		} else {
+			res.status(404).json({
+				status: 404,
+				message: "Plant not found",
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			status: 500,
+			message: error,
 		});
 	}
 });
