885bf79611df8afb6cbae197646442a350e64f60
oxypomme <oxytom8@gmail.com>
2022-01-21 17:04:38 +0100

    feat(frontend): :sparkles: Added plant deletion to backoffice

--- server/src/static/main.js
+++ server/src/static/main.js
@@ -1,34 +1,28 @@
-const URL = 'http://localhost:3000/api/plants';
+const API_URL = "http://localhost:3000/api/plants";
 
-const myVueApp =  Vue.createApp({
-    data () {
-        
-        return {
-            plants : []
-        };
-    },
-    methods: {
-        
-    },
-    computed : {
-        
-    },
-    async mounted(){
-        this.plants =  await (await fetch(URL)).json();
-    }
-})
-
-
-
-
-// myVueApp.component('', {
-//     data () {
-
-//     },
-
-//     template: `
-
-//     `
-// })
+const myVueApp = Vue.createApp({
+	data() {
+		return {
+			plants: [],
+			currentPlant: undefined,
+		};
+	},
+	methods: {
+		editPlant(plantId) {
+			this.currentPlant = plantId;
+		},
+		async deletePlant(plantId) {
+			await fetch(API_URL + "/" / plantId, {
+				method: "DELETE",
+			});
+			const index = this.plants.findIndex((p) => p.id === plantId);
+			this.plants.splice(index, 1);
+		},
+	},
+	computed: {},
+	async mounted() {
+		this.plants = await (await fetch(API_URL)).json();
+	},
+});
 
 myVueApp.mount("#container");
