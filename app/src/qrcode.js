9b708e8741e5e29af4bc58a9dbd6c24f4828e0ee
oxypomme <oxytom8@gmail.com>
2022-01-21 15:34:55 +0100

    refactor(frontend): :recycle: Splitted QRcode scan in a standalone file

---
+++ app/src/qrcode.js
@@ -0,0 +1,40 @@
+import { BarcodeScanner } from "@capacitor-community/barcode-scanner";
+
+/**
+ * Ouvre la camera et scan le QR-code
+ */
+const startScan = async () => {
+	BarcodeScanner.hideBackground();
+	const result = await BarcodeScanner.startScan();
+	if (result.hasContent) {
+		console.log(result.content);
+	}
+};
+
+/**
+ * Demande la permissions pour ouvrir la camera
+ * @returns
+ */
+const checkPermission = async () => {
+	// check or request permission
+	const status = await BarcodeScanner.checkPermission({ force: true });
+
+	if (status.granted) {
+		// the user granted permission
+		return true;
+	}
+
+	return false;
+};
+
+/**
+ * Demande à l'utilisateur s'il veut scanner un QRcode
+ */
+const askUser = async () => {
+	await checkPermission();
+	const c = confirm("Do you want to scan a barcode?");
+
+	if (c) {
+		startScan();
+	}
+};
