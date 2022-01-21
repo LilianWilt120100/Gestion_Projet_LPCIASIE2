97e60af076c650e670505ca9640cde20875a7dab
oxypomme <oxytom8@gmail.com>
2022-01-21 16:27:41 +0100

    feat(frontend): :sparkles: Can get plant from QRcode

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
 	entry: "./src/index.js",
+	mode: process.env.NODE_ENV ? "production" : "development",
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "www"),
